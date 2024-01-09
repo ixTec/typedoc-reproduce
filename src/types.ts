f58a2aec98359833299b755d86108f114fdd1ce2
ixtec <christian.dalby@datacamp.com>
2024-01-09 18:41:31 +0000

    remove old types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,8 @@
-export type CssVariable = `--${string}`;
-export type CssVariableFunc = `var(${CssVariable})`;
-
 export type PolymorphicRef<T extends React.ElementType> =
-  React.ComponentPropsWithRef<T>['ref'];
+  React.ComponentPropsWithRef<T>["ref"];
 
 export type PolymorphicComponentProps<
   T extends React.ElementType,
-  P extends Record<string, unknown>,
+  P extends Record<string, unknown>
 > = Omit<React.ComponentPropsWithoutRef<T>, keyof P> &
   P & { as?: T; ref?: PolymorphicRef<T> };
